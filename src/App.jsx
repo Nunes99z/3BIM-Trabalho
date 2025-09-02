76d37c91b1f4b0c3b227f1ad8478f0784c208a20
Lucas Nunes <lucas.nunes@eaportal.org>
2025-09-02 15:42:10 -0300

    primeiro salvamiento

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
@@ -8,28 +6,21 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+
+      <body>
+        <div class="card">
+          <img src="https://rickandmortyapi.com/api/character/avatar/1.jpeg" alt="Rick Sanchez">
+            <div class="info">
+              <h2>Rick Sanchez</h2>
+              <p><strong>Status:</strong> Vivo 🟢</p>
+              <p><strong>Espécie:</strong> Humano</p>
+              <p><strong>Último episódio:</strong> Ricksy Business</p>
+              <p><strong>Último local visto:</strong> Earth (Replacement Dimension)</p>
+            </div>
+        </div>
+      </body>
+          </>
+          )
 }
 
 export default App
