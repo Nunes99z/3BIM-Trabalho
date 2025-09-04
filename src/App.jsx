a2403cebb5ac965685928b4da4674a897a803b4e
lucas <lucas.nunes@eaportal.org>
2025-09-04 17:17:41 -0300

    primeiro salvamento

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,20 @@
-import { useState } from 'react'
-import './App.css'
+import React from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-
-      <body>
-        <div class="card">
-          <img src="https://rickandmortyapi.com/api/character/avatar/1.jpeg" alt="Rick Sanchez">
-            <div class="info">
-              <h2>Rick Sanchez</h2>
-              <p><strong>Status:</strong> Vivo 🟢</p>
-              <p><strong>Espécie:</strong> Humano</p>
-              <p><strong>Último episódio:</strong> Ricksy Business</p>
-              <p><strong>Último local visto:</strong> Earth (Replacement Dimension)</p>
-            </div>
-        </div>
-      </body>
-          </>
-          )
+    <div className="app">
+      <img
+        src="https://rickandmortyapi.com/api/character/avatar/1.jpeg"
+        alt="Rick Sanchez"
+      />
+      <h2>Rick Sanchez</h2>
+      <p><strong>Status:</strong> Vivo</p>
+      <p><strong>Espécie:</strong> Humano</p>
+      <p><strong>Gênero:</strong> Masculino</p>
+      <p><strong>Último episódio:</strong> Ep. 41 - "Rickmurai Jack"</p>
+    </div>
+  );
 }
 
-export default App
+export default App;
